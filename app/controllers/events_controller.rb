4a90c4639b322cf314c4079b9997071925876bf8
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-09-28 15:10:19 +0900

    コントローラーの作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,22 @@
 class EventsController < ApplicationController
+  def new
+    @event = Event.new
+    2.times { @event.schedules.build, @event.shops.build }
+  end 
+  
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:success] = 'Event created!'
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    
+    def event_params
+      params.require(:event).permit(:name, :description, schedule_attributes: [:savedate, :savetime], shop_attributes: [:shop_name, :shop_url, :map_url, :comment])
+    end
 end
