0408fc490dda6d2eaa087e63609f56d3e5803623
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-09-28 22:27:27 +0900

    バリデーションの確認

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,6 @@
 class Schedule < ApplicationRecord
   belongs_to :event, inverse_of: :schedules
   validates_presence_of :event
+
+  validates :savedate, presence: true
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,6 @@
 class Shop < ApplicationRecord
   belongs_to :event, inverse_of: :shops
   validates_presence_of :event
+
+  validates :shop_name, presence: true
 end
