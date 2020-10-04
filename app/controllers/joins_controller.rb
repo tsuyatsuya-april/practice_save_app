34d01f664837b6d52d8f0b13b83d413623a6cb5b
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-10-04 10:24:59 +0900

    ジョインコントローラーのレンダー設定

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -5,6 +5,17 @@ class JoinsController < ApplicationController
 
 
   def create
-    
+    @join = Join.new(join_params)
+    if @join.save
+      flash[:success] = 'join created!'
+      redirect_to event_path(:event_id)
+    else
+      @event = Event.find(params[:event_id])
+      render "events/show"
+    end
+  end
+
+  def join_params
+    params.require(:join).permit(:nickname, date_answers_attributes: [:schedule_id, :status], shop_answers_attributes: [:shop_id, :status])
   end
 end
